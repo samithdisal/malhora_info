4bd84dbb30d681605aa7a62a02e5f72cf4f61ddb
Samith Sandanayake <samiths@zone24x7.com>
2017-03-20 08:39:12 +0530

    initial application

---
+++ api/routes.go
@@ -0,0 +1,8 @@
+package api
+
+import "github.com/gorilla/mux"
+
+// Configure configures API routes
+func Configure(r *mux.Router) {
+
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"log"
+
+	"fmt"
+
+	"github.com/gorilla/mux"
+	"github.com/samithdisal/malhora_info/api"
+)
+
+const apiRoutesPrefix = "/api/v1"
+
+// APIRouter handles API reqursts
+var APIRouter = mux.NewRouter()
+
+func main() {
+
+	port := flag.Int("port", 8090, "Port to serve http content")
+
+	api.Configure(APIRouter)
+
+	http.Handle(apiRoutesPrefix, APIRouter)
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+
+}
